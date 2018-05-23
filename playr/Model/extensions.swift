5b40bf076d9c078372cf3958c4780ffcc5665a2b
Michel Balamou <michelbalamou@gmail.com>
2018-05-23 17:56:07 -0400

    Removed old networking model

---
+++ playr/Model/extensions.swift
@@ -0,0 +1,45 @@
+//
+//  extensions.swift
+//  playr
+//
+//  Created by Michel Balamou on 5/23/18.
+//  Copyright © 2018 Michel Balamou. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// SOURCE: https://stackoverflow.com/questions/24231680/loading-downloading-image-from-url-on-swift
+extension UIImageView {
+    
+    func loadCache(link: String, contentMode mode: UIViewContentMode)
+    {
+        let url = URL(string: link)!
+        self.contentMode = mode
+        self.kf.setImage(with: url)
+    }
+    
+    
+    func getImgFromUrl(link: String, contentMode mode: UIViewContentMode) {
+        let url = URL(string: link)
+        self.contentMode = mode
+        
+        DispatchQueue.global().async {
+            let data = try? Data(contentsOf: url!) //make sure your image in this url does exist, otherwise unwrap in a if let check / try-catch
+            
+            DispatchQueue.main.async {
+                if let d = data {
+                    self.image = UIImage(data: d)
+                }
+            }
+        }
+    }
+}
+
+extension CGRect {
+    
+    init(_ x:CGFloat, _ y:CGFloat, _ w:CGFloat, _ h:CGFloat) {
+        
+        self.init(x:x, y:y, width:w, height:h)
+    }
+}
